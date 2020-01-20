57199fb6b217b46b4db93d1ed798ebed746e613d
UncleDrew-11 <1035650839@qq.com>
2020-01-20 15:38:31 +0800

    上传代码

---
+++ 88.合并两个有序数组/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);//排序
+    }//从nums2中复制，0开始，到nums1的m位置，复制n个
+
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+    }
+}
