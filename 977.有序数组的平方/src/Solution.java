01c3c509e66dd3b6cf1fbb099fce8b55a98dbe32
UncleDrew-11 <1035650839@qq.com>
2020-01-30 17:31:09 +0800

    上传代码

---
+++ 977.有序数组的平方/src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int[] B = new int[A.length];
+        System.arraycopy(A, 0, B, 0, A.length);
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i] * A[i];
+        }
+        Arrays.sort(B);
+        return B;
+    }
+}
