c2d9ef1aed0e2ea9ad0bc69dc7d1dd576e52c90b
UncleDrew-11 <1035650839@qq.com>
2020-02-23 17:26:31 +0800

    上传代码

---
+++ 110. 平衡二叉树/src/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.abs(left - right) < 2
+                && isBalanced(root.left)
+                && isBalanced(root.right);
+    }
+}
