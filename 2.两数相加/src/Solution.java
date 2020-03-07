64280f17cbd88ed72d67c883fa735798026e8f71
UncleDrew-11 <1035650839@qq.com>
2020-03-07 09:07:15 +0800

    上传代码

---
+++ 2.两数相加/src/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ret = new ListNode(0);
+        ListNode cur = ret;
+        ListNode p = l1;
+        ListNode q = l2;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return ret.next;
+    }
+}
