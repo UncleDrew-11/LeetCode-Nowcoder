33044d1417818cc3349d37eb088e5685766ecf13
UncleDrew-11 <1035650839@qq.com>
2020-04-17 21:48:24 +0800

    上传代码

---
+++ 洗牌/src/test.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * 每次读取一个数之后，算出他经过k次洗牌后的位置，只用一个长度为2n数组用来输出
+ * 根据当前数的位置，可以算出经过一次洗牌后的位置
+ * 如果当前数小于等于n（即在左手），则他下次出现的位置是 2*当前位置-1
+ * 如果当前位置大于n（即在右手）,则他下次出现的位置是 2*（当前位置 - n）
+ * 个人建议在线面试题的时候如果5分钟内没想到好方法建议就使用暴力方法，
+ * 毕竟测试用例不通过什么都没用
+ * Created by lizo on 2016/8/20.
+ */
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int groups = sc.nextInt();
+        while (groups-- > 0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] res = new int[2*n];
+            for(int i=0;i<2*n;i++){
+                int tmp = i + 1;
+                for(int j = 0; j < k;j++){
+                    if (tmp <= n) tmp = 2*tmp - 1;
+                    else tmp = 2 * (tmp - n);
+                }
+                res[tmp - 1]=sc.nextInt();
+            }
+            //输出
+            if(res.length> 0) System.out.print(res[0]);
+            for(int i = 1;i< 2*n;i++){
+                System.out.print(" "+res[i]);
+            }
+            System.out.println();
+        }
+    }
+}
