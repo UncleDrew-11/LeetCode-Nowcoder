ecedecba047507d8373b526bde54cc5e39e58a7e
UncleDrew-11 <1035650839@qq.com>
2020-05-09 16:53:51 +0800

    上传代码

---
+++ 数字和为sum的方法数/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int sum = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+        }
+    }
+}
