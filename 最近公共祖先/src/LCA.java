d80fba6a6be841037d666b5a8dc296b03c413adc
UncleDrew-11 <1035650839@qq.com>
2020-05-30 23:23:19 +0800

    上传代码

---
+++ 最近公共祖先/src/LCA.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LCA {
+    public int getLCA(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a /= 2;
+            } else {
+                b /= 2;
+            }
+        }
+        return a;
+    }
+}
