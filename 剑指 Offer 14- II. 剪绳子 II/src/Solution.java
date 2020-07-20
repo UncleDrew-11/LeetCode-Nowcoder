04361526a22303310725c7170ab1071664821bd5
UncleDrew-11 <1035650839@qq.com>
2020-07-20 14:37:57 +0800

    上传代码

---
+++ 剑指 Offer 14- II. 剪绳子 II/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int cuttingRope(int n) {
+        if(n == 2)
+            return 1;
+        if(n == 3)
+            return 2;
+        long res = 1;
+        while(n > 4){
+            res *= 3;
+            res = res % 1000000007;
+            n -= 3;
+        }
+        return (int)(res * n % 1000000007);
+    }
+}
