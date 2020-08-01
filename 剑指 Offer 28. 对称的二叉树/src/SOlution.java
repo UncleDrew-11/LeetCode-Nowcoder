33290fdca164cc2ed73433975b302476d3f7e281
UncleDrew-11 <1035650839@qq.com>
2020-08-01 10:18:45 +0800

    上传代码

---
+++ 剑指 Offer 28. 对称的二叉树/src/SOlution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return recur(root.left, root.right);
+    }
+
+    public boolean recur(TreeNode L, TreeNode R) {
+        if (L == null && R == null) {
+            return true;
+        }
+        if (L == null || R == null || L.val != R.val) {
+            return false;
+        }
+        return recur(L.left, R.right) && recur(L.right, R.left);
+    }
+}
