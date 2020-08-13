6d9ada7aa92724e09875b42bd38cf5751b66f495
UncleDrew-11 <1035650839@qq.com>
2020-08-13 17:21:18 +0800

    上传代码

---
+++ 剑指 Offer 54. 二叉搜索树的第k大节点/src/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int k;
+    int ret;
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return ret;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.right);
+        if (k == 0) {
+            return;
+        }
+        if (--k == 0) {
+            ret = root.val;
+        }
+        dfs(root.left);
+    }
+}
