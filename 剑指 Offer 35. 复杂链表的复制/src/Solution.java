86e3010800ecccd6f41928fdd2cede66fd4c9680
UncleDrew-11 <1035650839@qq.com>
2020-08-14 10:10:40 +0800

    上传代码

---
+++ 剑指 Offer 35. 复杂链表的复制/src/Solution.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+class Solution {
+    public Node copyRandomList(Node head) {
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        //添加结点到map中去
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        //添加关系
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
