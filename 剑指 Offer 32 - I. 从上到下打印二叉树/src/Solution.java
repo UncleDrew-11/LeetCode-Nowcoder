d3f6cbf658f86e54e1647ecc90cac5c2da874431
UncleDrew-11 <1035650839@qq.com>
2020-08-15 12:59:31 +0800

    上传代码

---
+++ 剑指 Offer 32 - I. 从上到下打印二叉树/src/Solution.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+class Solution {
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        List<Integer> list = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+}
