4b11068d9f262194c165e53ad026a5eeaa9bc25c
UncleDrew-11 <1035650839@qq.com>
2020-08-17 21:32:38 +0800

    上传代码

---
+++ 剑指 Offer 18. 删除链表的节点/src/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head.val == val) {
+            return head.next;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if (cur != null) {
+            pre.next = cur.next;
+        }
+        return head;
+    }
+}
